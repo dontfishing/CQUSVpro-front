1ada12487178c4c33ac4eb57728b13d38e4b35ed
dontfishing <zhywsdr@163.com>
2022-06-30 11:19:34 +0800

    完成了上传头像

--- CSI/App.vue
+++ CSI/App.vue
@@ -1,7 +1,23 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+		    console.log('App Launch');
+		    // #ifdef APP-PLUS
+		    // token标志来判断
+		    let token= uni.getStorageSync('login_token');
+				if (token) {
+		        //不存在则跳转至登录页
+		        uni.reLaunch({
+		            url: "/pages/login/login",
+			    success: () => {
+			        plus.navigator.closeSplashscreen();
+			    }
+			})
+		    } else {
+				//存在则关闭启动页进入首页
+				plus.navigator.closeSplashscreen();
+		    }
+		    // #endif
 		},
 		onShow: function() {
 			console.log('App Show')
